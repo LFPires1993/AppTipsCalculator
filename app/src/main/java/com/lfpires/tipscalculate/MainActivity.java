e7d711c8a6e35566228f2719d076b93bc4cadc6b
lucas.pires <lfpires1993@gmail.com>
2021-06-06 17:08:49 -0300

    Instantiating Seekbar Listener

--- app/src/main/java/com/lfpires/tipscalculate/MainActivity.java
+++ app/src/main/java/com/lfpires/tipscalculate/MainActivity.java
@@ -25,7 +25,23 @@ public class MainActivity extends AppCompatActivity {
         textViewFinalGorjeta         = findViewById(R.id.textViewFinalGorjeta);
         textViewFinalPreco           = findViewById(R.id.textViewFinalPreco);
 
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                porcentagem = progress;
+                textViewPorcentagem.setText("R$ " + progress);
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 }
